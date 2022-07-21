ddef9c41199f28b194bc33d8f51bc12730edb9bc
himanshu82817 <himanshu82817@gmail.com>
2022-07-21 15:49:24 +0530

    bod images

--- src/app/components/board-of-directors/board-of-directors.component.ts
+++ src/app/components/board-of-directors/board-of-directors.component.ts
@@ -14,26 +14,32 @@ export class BoardOfDirectorsComponent implements OnInit {
 
   bodCards =[
     {
+      img:'assets/images/bod/ajaykumar.jpg',
       name:'Ajay Kumar',
       about:'Non-Executive & Independent Director'
     },
     {
+      img:'assets/images/bod/AshokKumarGupta.jpg',
       name:'Ashok Kumar Gupta',
       about:'Managing Director'
     },
     {
+      img:'assets/images/bod/ssl.jpg',
       name:'Shyam Sunder Lal Gupta',
       about:'Non-Executive & Independent Director'
     },
     {
+      img:'assets/images/bod/GovindPrasadAgrawal.jpg',
       name:'Govind Prasad Agrawal',
       about:'Non-Executive Chairman'
     },
     {
+      img:'assets/images/bod/Bhupinder Singh.jpg',
       name:'Bhupinder Singh',
       about:'Non-Executive & Independent Director'
     },
     {
+      img:'assets/images/bod/ASHU GUPTA.jpg',
       name:'Ashu Gupta',
       about:'Non-Executive Director'
     },
